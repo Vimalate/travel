3bd9557f80ada9675db2e75b9885e05e3467c8e2
liujian <1902644369@qq.com>
2019-05-03 20:49:01 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="container">
+     Home
+ </div>
+</template>
+
+<script>
+
+export default {
+ data() {
+  return {
+
+  }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped lang="css">
+
+</style>
